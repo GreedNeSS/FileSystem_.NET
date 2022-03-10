e79be8f9b5868863cd8f183fdc8d673bdb29f3c6
GreedNeSS <ruslan199126@gmail.com>
2022-03-10 22:16:18 +0300

    Добавьте файлы проекта.

---
+++ Drives/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Drives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Drivers *****");
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            ShowDrivesInfo(drives);
+        }
+
+        static void ShowDrivesInfo(DriveInfo[] drives)
+        {
+            foreach (var drive in drives)
+            {
+                Console.WriteLine($"\nName: {drive.Name}");
+                Console.WriteLine($"Type: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Total size: {drive.TotalSize}");
+                    Console.WriteLine($"Total free space: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Volume Label: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
