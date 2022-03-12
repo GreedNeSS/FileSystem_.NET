5d375a4b11b63a8159921db84545d6caa0e6e988
GreedNeSS <ruslan199126@gmail.com>
2022-03-12 20:10:16 +0300

    Added BinaryWriter and BinaryReader examples.

---
+++ BinaryWriterAndBinaryReader/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryWriterAndBinaryReader
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
---
+++ BinaryWriterAndBinaryReader/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BinaryWriterAndBinaryReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** BinaryWriter And BinaryReader *****");
+
+            Person[] people =
+            {
+                new Person("Ruslan", 30),
+                new Person("Marcus", 40),
+                new Person("Henry", 28)
+            };
+            string path = @"F:\Text\Person.dat";
+            List<Person> personList = new List<Person>();
+
+            WriteBinaryFileAsync(path, people);
+            ReadBinaryFileAsync(path, personList);
+
+            Console.WriteLine("\nСодержимое personList:");
+            personList.ForEach(person => Console.WriteLine(person));
+        }
+
+        static void WriteBinaryFileAsync(string path, IEnumerable<Person> people)
+        {
+            Console.WriteLine("\n=> WriteBinaryFileAsync():");
+
+            using BinaryWriter writer = new(File.Open(path, FileMode.OpenOrCreate));
+
+            foreach (Person person in people)
+            {
+                writer.Write(person.Name);
+                writer.Write(person.Age);
+            }
+
+            Console.WriteLine($"Файл{path} записан!");
+        }
+
+        static void ReadBinaryFileAsync(string path, List<Person> resultList)
+        {
+            Console.WriteLine("\n=> ReadBinaryFileAsync():");
+
+            using BinaryReader reader = new(File.Open(path, FileMode.OpenOrCreate));
+
+            while (reader.PeekChar() > -1)
+            {
+                string name = reader.ReadString();
+                int age = reader.ReadInt32();
+                resultList.Add(new Person(name, age));
+            }
+
+            Console.WriteLine($"Файл{path} прочитан!");
+        }
+    }
+}
