4321aea80e1a33a4de4a039a1f70d8cbfaeec8bf
GreedNeSS <ruslan199126@gmail.com>
2022-03-13 19:38:27 +0300

    Added FileSystemWatcher examle.

---
+++ DirectoryWatcher/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+namespace DirectoryWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Directory Watcher *****");
+
+            FileSystemWatcher fsWatcher = new FileSystemWatcher();
+
+            try
+            {
+                fsWatcher.Path = @"F:\Text";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+
+            fsWatcher.NotifyFilter = NotifyFilters.FileName 
+                | NotifyFilters.DirectoryName
+                | NotifyFilters.CreationTime 
+                | NotifyFilters.LastWrite 
+                | NotifyFilters.LastAccess;
+
+            fsWatcher.Filter = "*.txt";
+
+            fsWatcher.Changed += (s, e) =>
+            {
+                Console.WriteLine($"{e.ChangeType}: {e.Name}, path: {e.FullPath}");
+            };
+
+            fsWatcher.Created += (s, e) =>
+            {
+                Console.WriteLine($"{e.ChangeType}: {e.Name}, path: {e.FullPath}");
+            };
+
+            fsWatcher.Deleted += (s, e) =>
+            {
+                Console.WriteLine($"{e.ChangeType}: {e.Name}, path: {e.FullPath}");
+            };
+
+            fsWatcher.Renamed += (s, e) =>
+            {
+                Console.WriteLine($"{e.ChangeType}: {e.Name}, path: {e.FullPath}");
+            };
+
+            fsWatcher.EnableRaisingEvents = true;
+
+            while (Console.Read() != 'q');
+        }
+    }
+}
